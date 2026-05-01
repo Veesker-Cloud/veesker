518ee49581c4cddce3a0560af4c0aa05ca59bc20
Geraldo Ferreira Viana Júnior <geefatec@gmail.com>
2026-04-30 23:15:57 -0300

    feat(engine): CLI 'encrypt' + 'decrypt' commands (X25519 envelope)

--- engine/src/cli/index.ts
+++ engine/src/cli/index.ts
@@ -4,6 +4,8 @@ import { registerInfo } from "./commands/info";
 import { registerCreate } from "./commands/create";
 import { registerQuery } from "./commands/query";
 import { registerKeypair } from "./commands/keypair";
+import { registerEncrypt } from "./commands/encrypt";
+import { registerDecrypt } from "./commands/decrypt";
 
 const program = new Command();
 program
@@ -15,6 +17,8 @@ registerInfo(program);
 registerCreate(program);
 registerQuery(program);
 registerKeypair(program);
+registerEncrypt(program);
+registerDecrypt(program);
 
 program.parseAsync(process.argv).catch((err) => {
   console.error(`error: ${(err as Error).message}`);
